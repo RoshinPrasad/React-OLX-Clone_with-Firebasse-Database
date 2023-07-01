3bd5562d001f77ec3f8bfead034ab5269b4de840
Roshin Prasad <88421254+RoshinPrasad@users.noreply.github.com>
2023-07-01 14:46:21 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,14 @@ import 'firebase/compat/firestore'
 import 'firebase/compat/auth'
 import 'firebase/compat/storage'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCDFDhw84KvouiWeT9Pz2Zu0Tmq_ajw0SY",
-  authDomain: "my-olx-b0c8f.firebaseapp.com",
-  projectId: "my-olx-b0c8f",
-  storageBucket: "my-olx-b0c8f.appspot.com",
-  messagingSenderId: "435580427298",
-  appId: "1:435580427298:web:4f25256b1bf03eb39bbe7b",
-  measurementId: "G-F3L3HCG2WB"
+const firebaseConfig = { (Take #this Configuration from Firebase Database)
+  apiKey: "--ADD Your API Key Here"
+  authDomain
+  projectId
+  storageBucket
+  messagingSenderId
+  appId
+  measurementId
 };
 
 export default firebase.initializeApp(firebaseConfig);
